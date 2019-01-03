cd6f569fa0c3f2c006654502d12c736508a66490
wotto27oct <wotto27oct@gmail.com>
2019-01-03 20:15:12 +0900

    making Batch file

---
+++ bin/runtest.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+gcc -std=c99 -o kakizome kakizome.c
+
+REM == main function
+call :runtest 0 0
+echo OK
+exit /b 
+
+REM == function "runtest"
+:runtest
+for /F "usebackq delims=" %%a in (`kakizome.exe %1`) do set output=%%a
+if not %output% == %2 (
+	echo %1: %2 expected, but got %output%
+	exit /B
+) 
+echo %1 =^> %output%
+
+exit /b
