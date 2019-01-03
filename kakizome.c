0e247a80f1a600a273b7b5560fd74651963e70a7
wotto27oct <wotto27oct@gmail.com>
2019-01-03 20:52:16 +0900

    making error message

--- kakizome.c
+++ kakizome.c
@@ -6,18 +6,33 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 static char *p;
 
+__attribute__((noreturn)) static void error(char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+static void skip(){
+	while (isspace(*p))
+		p++;
+}
+
 static int eval(){
+	skip();
 	if (isdigit(*p)){
 		int val = *p++ - '0';
 		while (isdigit(*p))
 			val = val * 10 + (*p++ - '0');
 		return val;
 	}
-	p++;
-	return 0;
+	error("invalid character: %c", *p);
 }
 
 int main(int argc, char **argv){
